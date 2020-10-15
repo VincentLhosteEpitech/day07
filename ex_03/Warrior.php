bf55f0179441a6a1818d5bbb4024676f929c5bb5
Alex <alexandre.macrel@epitech.eu>
2020-10-15 11:42:14 +0200

    Modif Warrior delete abstract

---
+++ ex_03/Warrior.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once("Character.php");
+
+class Warrior extends Character 
+{
+    Const CLASSE = "Warrior";
+    
+    public function __construct($name)
+    {   
+        parent:: __construct($name);
+        $this->name = $name;
+        $this->life = 100;
+        $this->agility = 10;
+        $this->strength = 8;
+        $this->wit = 3;
+
+        echo $this->name . ": I'll engrave my name in history!\n";
+    }
+    public function attack()
+    {
+        echo $this->name . ": I'll crush you with my hammer!\n";
+    }
+
+    public function __destruct()
+    {
+        echo $this->name . ": Aarrg I can't believe I'm dead...\n";
+    }
+}
+
+
+/*$warrior = new Warrior("Jean-Luc");
+$warrior->attack();*/
+
+$warrior = new Warrior("Jean-Luc");
+$warrior->moveRight();
+$warrior->moveLeft();
+$warrior->moveUp();
+$warrior->moveDown();
+
+?>
